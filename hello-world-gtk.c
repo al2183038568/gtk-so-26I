7ffcffb117651fed8e43b232be88d98ccc8b2f6b
usuario <usuario@usuario-VirtualBox.(none)>
2026-03-09 00:34:05 -0600

    Agregar hello-world-gtk

---
+++ hello-world-gtk.c
@@ -0,0 +1,31 @@
+#include <gtk/gtk.h>
+
+static void activate_cb(GtkApplication *app, gpointer user_data)
+{
+GtkWidget *window;
+GtkWidget *label;
+window = gtk_application_window_new(app);
+gtk_window_set_title(GTK_WINDOW(window), "Hello World!");
+gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+
+label = gtk_label_new("Hello World!");
+gtk_container_add(GTK_CONTAINER(window), label);
+
+gtk_widget_show_all(window);
+
+}
+
+int main(int argc, char **argv)
+{
+GtkApplication *app;
+int status;
+app = gtk_application_new("org.example.hello", G_APPLICATION_FLAGS_NONE);
+g_signal_connect(app, "activate", G_CALLBACK(activate_cb), NULL);
+
+status = g_application_run(G_APPLICATION(app), argc, argv);
+
+g_object_unref(app);
+
+return status;
+
+}
